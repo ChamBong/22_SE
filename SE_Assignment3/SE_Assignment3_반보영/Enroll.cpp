d3e1e04871c38ad7396eeb1bb5ece16f49c890aa
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-06-02 21:16:14 +0900

    create classes related to the sale use case

---
+++ SE_Assignment3/SE_Assignment3_반보영/Enroll.cpp
@@ -0,0 +1,88 @@
+#include "hong_ping.h"
+
+// Initializing Static Attribute
+EnrollUI* EnrollUI::enrollUI = nullptr;
+Enroll* Enroll::enroll = nullptr;
+
+/*
+ *  Function Name :
+ *  Parameters    :
+ *  Return Type   :
+ *  Description   : 
+ */
+EnrollUI* EnrollUI::getUiInstance()
+{
+    if(EnrollUI::enrollUI == nullptr)
+        EnrollUI::enrollUI = new EnrollUI;
+
+    return EnrollUI::enrollUI;
+}
+
+/*
+ *  Function Name :
+ *  Parameters    :
+ *  Return Type   :
+ *  Description   : 
+ */
+EnrollUI::EnrollUI()
+{
+    // NOP
+}
+
+/*
+ *  Function Name :
+ *  Parameters    :
+ *  Return Type   :
+ *  Description   : 
+ */
+EnrollUI::~EnrollUI()
+{
+    delete EnrollUI::enrollUI;
+}
+
+/*
+ *  Function Name :
+ *  Parameters    :
+ *  Return Type   :
+ *  Description   : 
+ */
+Enroll* Enroll::getControlInstance()
+{
+    if (Enroll::enroll == nullptr)
+        Enroll::enroll = new Enroll;
+
+    return Enroll::enroll;
+}
+
+/*
+ *  Function Name :
+ *  Parameters    :
+ *  Return Type   :
+ *  Description   : 
+ */
+EnrollUI* Enroll::getUI()
+{
+    return this->enrollUI;
+}
+
+/*
+ *  Function Name :
+ *  Parameters    :
+ *  Return Type   :
+ *  Description   : 
+ */
+Enroll::Enroll()
+{
+    this->enrollUI = EnrollUI::getUiInstance();
+}
+
+/*
+ *  Function Name :
+ *  Parameters    :
+ *  Return Type   :
+ *  Description   : 
+ */
+Enroll::~Enroll()
+{
+    delete this->enroll;
+}
